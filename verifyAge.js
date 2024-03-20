cf507bf3182c4fb3f063f7baede4dbe21a4af0c7
Mark Richter Jr <mark.richter93@gmail.com>
2024-03-20 16:29:11 -0500

    Update verifyAge.js

--- verifyAge.js
+++ verifyAge.js
@@ -1,10 +1,10 @@
 
 function verifyAdulthood(){
     const firstName = "Mark";
-const age = 30; 
+    const age = 30; 
 
     if (age >= 18)  return (`Welcome, ${firstName}`);
-    else return (`You are not old enough to enter, ${firstName}`);
+    return (`You are not old enough to enter, ${firstName}`);
 }
 
-console.log (verifyAdulthood)
+console.log (verifyAdulthood);
