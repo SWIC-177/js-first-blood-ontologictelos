7329e9d15ad04e88068c7156fd7bc6856189f230
Mark Richter Jr <mark.richter93@gmail.com>
2024-03-21 13:38:44 -0500

    Fix code add dumb ()

--- verifyAge.js
+++ verifyAge.js
@@ -1,10 +1,11 @@
+// no variable shadowing therefore everything for this assignment must be within the function scope (inside the {})
+// 
 
 function verifyAdulthood(){
     const firstName = "Mark";
-    const age = 30; 
-
+    const age = 30;
     if (age >= 18)  return (`Welcome, ${firstName}`);
     return (`You are not old enough to enter, ${firstName}`);
 }
 
-console.log (verifyAdulthood);
+console.log (verifyAdulthood());
