e742de0577d69add0654e54eb6c436a46e1d03cc
Mark Richter Jr <mark.richter93@gmail.com>
2024-03-26 21:42:54 -0500

    Nest if else statments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,15 @@
 // print numbers 1-100 to console
+// multiples of 3 print fizz
+// multiples of 5 print buzz
+// how do i replace a number with a string?
 
-let i
-
-for(i=1;i<=100;i=i+1 ) 
-{
-  console.log(i)
+for(let i=0;i<=100;i+=1) {
+if (i%3 ===0 && i%5 ===0){
+    console.log('fizzbuzz')
+  } else if (i%3 === 0) {
+  console.log('fizz')
+  } else if (i%5 ===0){
+    console.log('buzz')
+  } 
+  else {console.log(i)}
 }
