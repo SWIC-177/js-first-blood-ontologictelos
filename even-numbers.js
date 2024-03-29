243f45b5d8c16678294feca23db65846dff1bb0f
Mark Richter Jr <mark.richter93@gmail.com>
2024-03-28 19:56:59 -0500

    Update even-numbers.js

--- even-numbers.js
+++ even-numbers.js
@@ -1,7 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Issue 1: The for loop condition should have a terminating condition
-// Issue 2: The semicolon after the if condition should be removed
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i]%2 === 0) {
     console.log(numbers[i]);
