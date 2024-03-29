8f5cbe7ea0d55dfa0087fe836bce4be828e3136a
Mark Richter Jr <mark.richter93@gmail.com>
2024-03-28 21:08:42 -0500

    Add loop for guest age verification

---
+++ guest-list.js
@@ -0,0 +1,38 @@
+const people = [
+    {
+      name: "John",
+      age: 17,
+    },
+    {
+      name: "Jane",
+      age: 20,
+    },
+    {
+      name: "Mary",
+      age: 25,
+    },
+    {
+      name: "Peter",
+      age: 30,
+    },
+    {
+      name: "Paul",
+      age: 35,
+    },
+  ];
+  
+  function verifyAdulthood(personName, personAge) {
+    if (personAge >= 18) return `Welcome, ${personName}!`;
+  
+    return `You are not old enough to enter, ${personName}.`;
+  }
+  
+
+for(let i =0; i< people.length; i += 1){
+    const person = people[i];
+  const adultMessage = verifyAdulthood(person.name, person.age);
+  console.log(adultMessage);
+}
+
+  // create a loop that prints people to terminal.
+  // TODO: Use a loop to call the verifyAdulthood function for each person in the people array.
