f9bc2eb8c5571ef5a7307402ef9a938c4f8aeb22
Mark Richter Jr <mark.richter93@gmail.com>
2024-04-06 21:42:10 -0500

    Add example 1-3 as playground

---
+++ example2.js
@@ -0,0 +1,14 @@
+// explain how code works
+// explain pass by copy/value
+
+const younger1 = 13;
+let younger2 = younger1;
+
+function incrementAge(age) {
+  return age + 1;
+}
+
+younger2 = incrementAge(younger2);
+
+console.log(`Younger1 is: ${younger1}`);
+console.log(`Younger2 is: ${younger2}`);
