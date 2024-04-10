43815cb2a6ba86235597d6fc506699714f3b4439
Mark Richter Jr <mark.richter93@gmail.com>
2024-04-09 22:02:21 -0500

    Add week12.js alter code

---
+++ week-12.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "John Doe",
+    age: 23,
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      state: "NY",
+      zip: "10001",
+    },
+  };
+  
+  // Create a new object with the same properties as the original object.
+  const otherPerson = { ...person, hobbies: ["reading"]};
+  
+  otherPerson.name = "Jane Doe";
+  otherPerson.age = "25";
+  
+  console.log(`Person's name is: ${person.name}`);
+  console.log(`Other Person's name is: ${otherPerson.hobbies}`);
